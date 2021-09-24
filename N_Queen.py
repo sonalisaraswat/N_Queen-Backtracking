45c4215d9fd07562c8554a050412efa2107393ee
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2021-09-25 02:10:28 +0530

    Update N_Queen.py

--- N_Queen.py
+++ N_Queen.py
@@ -1,10 +1,10 @@
 def isSafe(arr,row,col):
-    #checking vertically through all rows of given col
+                                               #checking vertically through all rows of given col
     for e in range(N):
         if arr[e][col] == 'Q':
             return False
 
-    #checking upper left
+                                               #checking upper left
     r_up = row-1
     c_left = col-1
     while (r_up >= 0) and (c_left >= 0):
@@ -13,7 +13,7 @@ def isSafe(arr,row,col):
         r_up -= 1
         c_left -= 1
 
-    #checking upper right
+                                               #checking upper right
     r_up = row-1
     c_right = col+1
     while (r_up >= 0) and (c_right < N):
@@ -25,21 +25,23 @@ def isSafe(arr,row,col):
     return True
 
 
-# going through all rows
+                                               # going through all rows
 def N_Queen(arr,row):
     
-    if row == N:
+    if row == N:                              # BASE CONDITION: all rows checked so return True
         return True
     
-    for C in range(N):
-        if (isSafe(arr,row,C)):
-            arr[row][C] = 'Q'
-            if (N_Queen(arr,row+1)):
+    for C in range(N):                        # Check for all cols in a given row.
+        if (isSafe(arr,row,C)):               # if we get a safe col in the given row.
+            arr[row][C] = 'Q'                 # assign that col in the given row as Q.
+            if (N_Queen(arr,row+1)):          # run N queen for next set of rows.
                 return True
-            arr[row][C] = 'x'
+            arr[row][C] = 'x'                 # BACKTRACKING POINT: if any next rows return False: set the col of that row back to X.
+    
     return False
 
-# main start
+
+                                              # main start
 N = int(input())
 
 arr = []
